202ae2e1804f77e8319d80f075f6b580636d05de
aydenja-iastate <aydenja@iastate.edu>
2022-04-21 07:08:13 -0500

    test

---
+++ src/comp/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+
+export const Header = () => {
+  return (
+    <div> 
+      <ul>
+        <li><h1>Ayden Albertsen</h1></li>
+        <li><a href="https://www.google.com/">About</a></li>
+      </ul> 
+    </div>
+  )
+}
+
+export default Header
+
+//rafce 
